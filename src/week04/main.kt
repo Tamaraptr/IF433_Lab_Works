3c67081a45854d104cb50a13a537587582af0512
tamaratrislwa <tamara.putri@student.umn.ac.id>
2026-02-25 15:53:00 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/week04/main.kt
+++ src/week04/main.kt
@@ -2,32 +2,20 @@ package week04
 
 fun main() {
 
-    println("--- Testing Vehicle ---")
-    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
-    generalVehicle.honk()
-    generalVehicle.accelerate()
-
-    println("\n--- Testing Car ---")
-    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
-    myCar.openTrunk()
-    myCar.honk()
-    myCar.accelerate()
-
-    println("\n--- Testing ElectricCar ---")
-    val ev = ElectricCar("Wuling", 4, 80)
-    ev.accelerate()   // dari ElectricCar (final override)
-    ev.honk()         // diwarisi dari Car
-    ev.openTrunk()    // diwarisi dari Car
-
-    val emp = Employee("Andi", 5_000_000)
-    emp.work()
-    println(emp.calculateBonus())
-
-    val manager = Manager("Budi", 10_000_000)
+    println("--- Testing Manager ---")
+    val manager = Manager(
+        name = "Budi",
+        baseSalary = 10_000_000
+    )
     manager.work()
-    println(manager.calculateBonus())
+    println("Bonus Manager: ${manager.calculateBonus()}")
 
-    val dev = Developer("Siti", 8_000_000, "Kotlin")
-    dev.work()
-    println(dev.calculateBonus())
+    println("\n--- Testing Developer ---")
+    val developer = Developer(
+        name = "Siti",
+        baseSalary = 8_000_000,
+        programmingLanguage = "Kotlin"
+    )
+    developer.work()
+    println("Bonus Developer: ${developer.calculateBonus()}")
 }
