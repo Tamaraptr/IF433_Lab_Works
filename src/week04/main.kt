ac121d972e9cd963263be8bbcbeacd615bced278
tamaratrislwa <tamara.putri@student.umn.ac.id>
2026-02-25 15:41:05 +0700

    week04: (task 1) test ElectricCar in main

--- src/week04/main.kt
+++ src/week04/main.kt
@@ -9,9 +9,13 @@ fun main() {
 
     println("\n--- Testing Car ---")
     val myCar = Car(brand = "Toyota", numberOfDoors = 4)
-    myCar.openTrunk()     // memanggil method milik Car
-    myCar.honk()          // memanggil method hasil override
-    myCar.accelerate()    // memanggil gabungan Parent + Child
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+
+    println("\n--- Testing ElectricCar ---")
     val ev = ElectricCar("Wuling", 4, 80)
-    ev.accelerate()
+    ev.accelerate()   // dari ElectricCar (final override)
+    ev.honk()         // diwarisi dari Car
+    ev.openTrunk()    // diwarisi dari Car
 }
