c8761596d0e7d20944a6fbecb40f45447dc3a567
tamaratrislwa <tamara.putri@student.umn.ac.id>
2026-02-25 15:29:14 +0700

    week04: test Vehicle and Car in main

--- src/week04/main.kt
+++ src/week04/main.kt
@@ -1,6 +1,15 @@
 package week04
 
 fun main() {
-    val car = Car("Honda", 4)
-    car.accelerate()
+
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()     // memanggil method milik Car
+    myCar.honk()          // memanggil method hasil override
+    myCar.accelerate()    // memanggil gabungan Parent + Child
 }
