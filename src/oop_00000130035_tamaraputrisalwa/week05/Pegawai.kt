016a3201c1228b18f006d30849925b55bb547c55
tamaratrislwa <tamara.putri@student.umn.ac.id>
2026-03-04 14:56:38 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000130035_tamaraputrisalwa/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000130035_tamaraputrisalwa.week05
+
+abstract class Pegawai(val nama: String) {
+    //Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
