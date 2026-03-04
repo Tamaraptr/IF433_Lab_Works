3e235b245758dd604907827d3a567b555546cfbe
tamaratrislwa <tamara.putri@student.umn.ac.id>
2026-03-04 15:04:39 +0700

    week05: create Dosen class inheriting Pegawai

--- src/oop_00000130035_tamaraputrisalwa/week05/Dosen.kt
+++ src/oop_00000130035_tamaraputrisalwa/week05/Dosen.kt
@@ -1,2 +1,13 @@
 package oop_00000130035_tamaraputrisalwa.week05
 
+class Dosen(nama: String, val nind: String) : Pegawai(nama) {
+    //WAJIB di-override karena fungsi bekerja() bersifat abstract di Parent
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+
+    // Fungsi unik/spesifik yang dimiliki Dosen
+    fun mengajar() {
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
+}
