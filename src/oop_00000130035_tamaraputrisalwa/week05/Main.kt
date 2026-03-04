708d5d861d31555550e2dca0fac911fdaeea7b70
tamaratrislwa <tamara.putri@student.umn.ac.id>
2026-03-04 16:59:08 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_00000130035_tamaraputrisalwa/week05/Main.kt
@@ -0,0 +1,28 @@
+package oop_00000130035_tamaraputrisalwa.week05
+
+fun main() {
+    val dosen1 = Dosen( nama = "Pak Alex", nind ="0123456")
+    val admin1 = Admin( nama = "Bu Siti")
+
+    // Polymorphic Collection List yang berisi tipe Parent, tapi isisnya object anak
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIFITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        // Pemanggilan Runtime Polymorphism
+        pegawai.bekerja()
+
+        // pegawai.mengajar() // INI AKAN EROR karena tipe referensinya adalah pegawai
+        // Smart Casting dengan is dan when
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: $pegawai.nidn)")
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("---------------------------------")
+    }
+}
--- src/oop_00000130035_tamaraputrisalwa/week05/Pegawai.kt
+++ src/oop_00000130035_tamaraputrisalwa/week05/Pegawai.kt
@@ -1,6 +1,6 @@
 package oop_00000130035_tamaraputrisalwa.week05
 
-abstract class Pegawai(val nama: String) {
+abstract  class Pegawai(val nama: String) {
     //Abstract function tidak memiliki body (tanpa kurung kurawal)
     abstract fun bekerja()
 }
