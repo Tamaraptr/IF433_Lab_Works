fb64dab524e7bd04c74b017d59469a6e42bba49e
tamaratrislwa <tamara.putri@student.umn.ac.id>
2026-03-11 20:18:02 +0700

    week06: (trial) create Clickable interface with backing field error

---
+++ src/oop_00000130035_tamaraputrisalwa/week06/Clickable.kt
@@ -0,0 +1,6 @@
+package oop_00000130035_tamaraputrisalwa.week06
+
+interface Clickable {
+    val name: String // Abstract property, tidak ada nilai default
+    fun click()
+}
---
+++ src/oop_00000130035_tamaraputrisalwa/week06/Watch.kt
@@ -0,0 +1,5 @@
+package oop_00000130035_tamaraputrisalwa.week06
+
+abstract class Watch {
+    abstract fun showTime()
+}
