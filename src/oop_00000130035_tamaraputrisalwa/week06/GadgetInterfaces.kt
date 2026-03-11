5619003bdf48581f8f628e4bd5d75f3454ede28c
tamaratrislwa <tamara.putri@student.umn.ac.id>
2026-03-11 20:39:30 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000130035_tamaraputrisalwa/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000130035_tamaraputrisalwa.week06
+
+interface  Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
