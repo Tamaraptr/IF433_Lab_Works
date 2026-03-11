d79573ad193e97a50dafe468bb02c1b60a5401ca
tamaratrislwa <tamara.putri@student.umn.ac.id>
2026-03-11 21:21:10 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000130035_tamaraputrisalwa/week06/Main.kt
+++ src/oop_00000130035_tamaraputrisalwa/week06/Main.kt
@@ -2,16 +2,22 @@ package oop_00000130035_tamaraputrisalwa.week06
 
 fun main() {
 
-    val hub = SmartHomeHub()
-
+    // Membuat perangkat
     val lamp = SmartLamp("L01", "Ruang Tamu")
     val speaker = SmartSpeaker("S01", "Google Nest Dapur")
     val cctv = SmartCCTV("C01", "Ezviz Garasi")
 
+    // Membuat SmartHomeHub
+    val hub = SmartHomeHub()
+
+    // Menambahkan device ke hub
     hub.addDevice(lamp)
     hub.addDevice(speaker)
     hub.addDevice(cctv)
 
-    println("\nMengaktifkan Security Mode...")
+    println("\n=== Aktivasi Security Mode ===")
     hub.activateSecurityMode()
+
+    println("\n=== Mematikan Semua Perangkat ===")
+    hub.turnOffAllSwitches()
 }
